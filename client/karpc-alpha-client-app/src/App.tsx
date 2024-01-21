4fd007fc50dbfee29714e9638921208342a86d02
RandonSRProctor <RandyProctor@Gmail.com>
2024-01-21 15:16:25 -0500

    updating config files.  Added prettierrc, but not sure if it is working yet.

--- client/karpc-alpha-client-app/src/App.tsx
+++ client/karpc-alpha-client-app/src/App.tsx
@@ -1,30 +1,30 @@
-import HamburgerIcon from './assets/hamburger-svgrepo-com.svg';
-import UserIcon from './assets/user-profile-svgrepo-com.svg';
+import HamburgerIcon from "./assets/hamburger-svgrepo-com.svg";
+import UserIcon from "./assets/user-profile-svgrepo-com.svg";
 
 const mockConversationData = [
   {
     index: 1,
-    timestamp: 'April, 20th, 1969',
-    content: 'Hey Randy',
-    user: 'Khalil',
+    timestamp: "April, 20th, 1969",
+    content: "Hey Randy",
+    user: "Khalil",
   },
   {
     index: 2,
-    timestamp: 'April, 20th, 1969',
-    content: 'Hey Khalil',
-    user: 'Randy',
+    timestamp: "April, 20th, 1969",
+    content: "Hey Khalil",
+    user: "Randy",
   },
   {
     index: 3,
-    timestamp: 'April, 20th, 1969',
+    timestamp: "April, 20th, 1969",
     content: "What's up?'",
-    user: 'Khalil',
+    user: "Khalil",
   },
   {
     index: 4,
-    timestamp: 'April, 20th, 1969',
-    content: 'Making an app my dude!',
-    user: 'Randy',
+    timestamp: "April, 20th, 1969",
+    content: "Making an app my dude!",
+    user: "Randy",
   },
 ];
 
@@ -43,13 +43,13 @@ function App() {
           </h1>
           <p className="text-s italic text-yellow-200">Public Conversation</p>
         </header>
-        <div className="pr-2 pt-2">
+        <div className="pt-2 pr-2 ">
           <img className="w-11 h-11" src={UserIcon} />
         </div>
       </div>
       <div className="APP_CONTENT flex justify-center p-2">
         <div className="CONVERSATION_CONTAINER w-1/2 border border-black rounded p-2">
-          {mockConversationData.map(message => {
+          {mockConversationData.map((message) => {
             return (
               <p className="text-base">
                 <span>{`${message.user}: `}</span>
