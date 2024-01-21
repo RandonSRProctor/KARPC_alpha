0f30962c5446d1d583b110c4ba9b75f3ea0eea2b
RandonSRProctor <RandyProctor@Gmail.com>
2024-01-21 16:23:36 -0500

    Set up mock conversation.

--- client/karpc-alpha-client-app/src/App.tsx
+++ client/karpc-alpha-client-app/src/App.tsx
@@ -3,39 +3,39 @@ import UserIcon from "./assets/user-profile-svgrepo-com.svg";
 
 const mockConversationData = [
   {
-    index: 1,
-    timestamp: "April, 20th, 1969",
-    content: "Hey Randy",
-    user: "Khalil",
+    id: 1,
+    created: "2024-01-21T21:09:32.000Z",
+    user: "randy",
+    message_text: "Hey Khalil!",
   },
   {
-    index: 2,
-    timestamp: "April, 20th, 1969",
-    content: "Hey Khalil",
-    user: "Randy",
+    id: 2,
+    created: "2024-01-21T21:09:32.000Z",
+    user: "khalil",
+    message_text: "Hey Khalil!",
   },
   {
-    index: 3,
-    timestamp: "April, 20th, 1969",
-    content: "What's up?'",
-    user: "Khalil",
+    id: 3,
+    created: "2024-01-21T21:09:32.000Z",
+    user: "randy",
+    message_text: "What are you up to?",
   },
   {
-    index: 4,
-    timestamp: "April, 20th, 1969",
-    content: "Making an app my dude!",
-    user: "Randy",
+    id: 4,
+    created: "2024-01-21T21:09:32.000Z",
+    user: "khalil",
+    message_text: "Some pair programming!",
   },
 ];
 
 function App() {
   return (
     <>
-      <div className="sticky top-0 bg-indigo-800 py-1 flex justify-between">
+      <div className="sticky top-0 flex justify-between bg-indigo-800 py-1">
         <div className="pl-2 pt-1">
-          <img className="w-11 h-11" src={HamburgerIcon} />
+          <img className="h-11 w-11" src={HamburgerIcon} />
         </div>
-        <header className="bg-indigo-800 text-white text-center">
+        <header className="bg-indigo-800 text-center text-white">
           <h1 className="flex items-center justify-center">
             <span className="text-3xl">K</span>
             <span className="text-base">&</span>
@@ -43,20 +43,21 @@ function App() {
           </h1>
           <p className="text-s italic text-yellow-200">Public Conversation</p>
         </header>
-        <div className="pt-2 pr-2 ">
-          <img className="w-11 h-11" src={UserIcon} />
+        <div className="pr-2 pt-2">
+          <img className="h-11 w-11" src={UserIcon} />
         </div>
       </div>
       <div className="APP_CONTENT flex justify-center p-2">
-        <div className="CONVERSATION_CONTAINER w-1/2 border border-black rounded p-2">
-          {mockConversationData.map((message) => {
-            return (
-              <p className="text-base">
-                <span>{`${message.user}: `}</span>
-                <span>{message.content}</span>
-              </p>
-            );
-          })}
+        <div className="CONVERSATION_CONTAINER w-1/2 rounded border border-black p-2">
+          {mockConversationData &&
+            mockConversationData.map((message) => {
+              return (
+                <p className="text-base">
+                  <span>{`${message.user}: `}</span>
+                  <span>{message.message_text}</span>
+                </p>
+              );
+            })}
         </div>
       </div>
     </>
