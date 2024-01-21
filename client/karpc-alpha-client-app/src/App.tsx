9e6ca62c6392a822587e325d247511c6ed1b32fa
RandonSRProctor <RandyProctor@Gmail.com>
2024-01-21 16:48:59 -0500

    Final little design tweaks.

--- client/karpc-alpha-client-app/src/App.tsx
+++ client/karpc-alpha-client-app/src/App.tsx
@@ -28,17 +28,19 @@ const mockConversationData = [
   },
 ];
 
+window.document.body.classList.add("bg-sky-500");
+
 function App() {
   return (
     <>
-      <div className="sticky top-0 flex justify-between bg-indigo-800 py-1">
+      <div className="sticky top-0 flex justify-between border-b border-indigo-900 bg-indigo-800 py-1 shadow">
         <div className="pl-2 pt-1">
           <img className="h-11 w-11" src={HamburgerIcon} />
         </div>
         <header className="bg-indigo-800 text-center text-white">
           <h1 className="flex items-center justify-center">
-            <span className="text-3xl">K</span>
-            <span className="text-base">&</span>
+            <span className="text-3xl">K</span> &nbsp;
+            <span className="text-">&epsilon;</span> &nbsp;
             <span className="text-3xl">R</span>
           </h1>
           <p className="text-s italic text-yellow-200">Public Conversation</p>
@@ -48,7 +50,7 @@ function App() {
         </div>
       </div>
       <div className="APP_CONTENT flex justify-center p-2">
-        <div className="CONVERSATION_CONTAINER w-1/2 rounded border border-black p-2">
+        <div className="CONVERSATION_CONTAINER w-1/2 rounded border border-sky-500 bg-sky-300 p-2 shadow">
           {mockConversationData &&
             mockConversationData.map((message) => {
               return (
