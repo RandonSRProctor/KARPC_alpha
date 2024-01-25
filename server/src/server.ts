926528d67e50ba57dfe798aae2a98a017a81eb09
RandonSRProctor <RandyProctor@Gmail.com>
2024-01-25 13:56:51 -0500

    Small changes in the middle of code review.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,53 +1,54 @@
-import express from "express";
+import express from 'express';
 import {
   getMessage,
   createMessage,
   getFullConversation,
   getLastTenMessages,
-} from "./database.js";
-import cors from "cors";
+} from './database.js';
+import cors from 'cors';
 
-console.log("2.1 start server");
+console.log('2.1 start server');
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: 'http://localhost:5173',
   })
 );
 
 app.use(express.json());
 
-app.get("/karpc_alpha", async (req, res) => {
+app.get('/conversation', async (req, res) => {
   const conversation = await getFullConversation();
-  console.log("Sending conversation!");
+  console.log('Sending conversation!');
   res.send(conversation);
 });
 
-app.get("/karpc_alpha/:id", async (req, res) => {
+app.get('/conversation/:id', async (req, res) => {
   const id = Number(req.params.id);
   if (isNaN(id)) {
     // bad request
-    res.status(400).send("Invalid id.  Please provide a number.");
+    res.status(400).send('Invalid id.  Please provide a number.');
   } else {
     const message = await getMessage(id);
+    // TODO: Get this logic correct
     if (message) {
       // is OK
       res.status(200).json(message);
     } else {
-      res.status(404).send("Message not found");
+      res.status(404).send('Message not found');
     }
   }
 });
 
-app.get("/karpc_alpha", async (req, res) => {
+app.get('/conversation', async (req, res) => {
   const conversation = await getLastTenMessages();
   console.log(`Sending last ten messages!`);
   res.send(conversation);
 });
 
-app.post("/karpc_alpha/createMessage", async (req, res) => {
+app.post('/conversation/createMessage', async (req, res) => {
   try {
     const { user, message_text } = req.body;
     const newMessage = await createMessage(user, message_text);
@@ -55,11 +56,11 @@ app.post("/karpc_alpha/createMessage", async (req, res) => {
     //res.status(201) request fullfilled and new resource created
     res.status(201).send(newMessage);
   } catch (error) {
-    console.error("Error creating message:", error);
-    res.status(500).send("Internal Server Error");
+    console.error('Error creating message:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
 app.listen(8080, () => {
-  console.log("Server is listening on port 8080");
+  console.log('Server is listening on port 8080');
 });
